d6fce8d3f97dc88735cddc01b1a6e98f7496037f
shakhzod <shahzodatabayev3949@gmail.com>
2021-02-28 17:27:58 +0500

    few change

--- lib/src/core/better_player_controller.dart
+++ lib/src/core/better_player_controller.dart
@@ -974,3 +974,21 @@ class BetterPlayerController extends ChangeNotifier {
     }
   }
 }
+class UserWatchedChunk {
+  int end;
+  int start;
+
+  UserWatchedChunk({this.end, this.start});
+
+  UserWatchedChunk.fromJson(Map<String, dynamic> json) {
+    end = json['end'];
+    start = json['start'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['end'] = this.end;
+    data['start'] = this.start;
+    return data;
+  }
+}
